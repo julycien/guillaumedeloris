3cbb0e2570b85c72e260005851b8dd23e7c939fe
Guillaume Deloris <guillaumedeloris@outlook.fr>
2022-09-29 23:05:21 +0200

    mise a jour site portfolio avec cv pdf

--- boundaries/BienvenueIHM.php
+++ boundaries/BienvenueIHM.php
@@ -9,36 +9,37 @@
     </head>
 
     <body>
-        <div class="logoin">
-            <a href="https://www.linkedin.com/in/guillaume-deloris-developpeur/" target="blank">
-                <img src="../images/logoinfondnoir.png" alt="logo linked in"></a>
-        </div>
         <h1>Site portfolio</h1>
         <div class="hline"></div>
+        
         <h2>Bienvenue sur mon site portfolio !</h2>
+        <span class="logoin">
+            <a href="https://www.linkedin.com/in/guillaume-deloris-developpeur/" target="blank">
+                <img src="../images/logoinfondnoir.png" alt="logo linked in"></a>
+        </span>
+
         <div class="container">
-            <p>Ce site est destiné à présenter des réalisations que j'ai conçues et programmées, 
+
+            <p>Ce site est destiné à présenter des réalisations que j'ai conçues et développées, 
                 ainsi que des extraits de codage, et mon CV.</p>
             <ul>
                 <p><b><i><u>Site réalisé en PHP OOP : </u></i></b></p>
                 <li><b>www.msr-bike.fr : Magasin et atelier moto </b></li>
                 <ul>
-                    <li>Front office : <a href="https://msr-bike.fr/boundaries/Index.php" target="blank">www.msr-bike.fr à Saint Romain de Jalionas</a></li>
-                    <li>Back office : <a href="https://msr-bike.fr/msrBikeAdmin/boundaries/LoginIHM.php" target="blank">www.msr-bike.fr Administrateur</a></li>
+                    <li>Front office : <a href="https://msr-bike.fr/boundaries/Index.php" target="blank">www.msr-bike.fr </a></li>
+                    <li>Back office : <a href="https://msr-bike.fr/msrBikeAdmin/boundaries/LoginIHM.php" target="blank">www.msr-bike.fr</a> site Administrateur</li>
                 </ul><br>
-                Site de préproduction toujours visible à l'url suivante :<br>
-                <a href="https://julycien.alwaysdata.net/msrBike/boundaries/Index.php" target="blank">www.msr-bike.fr à Beynost</a></li>
-
-
                 <p><b><i><u>Site réalisé en PHP procédural :</u></i></b></p>
                 <li><b>Gestion d'une copropriété</b></li>
                 <ul>
                     <li>Front office : <a href="http://www.coprostej.fr/boundaries/identificationIHM.php" target="blank">www.coprostej.fr</a></li>
-                    <li>Back office : <a href="https://www.coprostej.fr/SyndicSainteJulie/boundaries/identification.php" target="blank">www.coprostej.fr Administrateur</a></li>
+                    <li>Back office : <a href="https://www.coprostej.fr/SyndicSainteJulie/boundaries/identification.php" target="blank">www.coprostej.fr </a>site Administrateur</li>
                 </ul>
-                <p><b><i><u>Liens:</u></i></b></p>
-                <li><a href="../cv/cv.html" target="blank">Mon CV</a></li>
+                <p><b><i><u>Liens :</u></i></b></p>
+                <li><a href="../pdf/CV_Guillaume_Deloris.pdf" target="blank">Mon CV</a></li>
                 <li><a href="https://github.com/julycien" target="blank">Extraits de codage sur Github</a></li>
+                 <p><b><i><u>Contact mail :</u></i></b></p>
+                <li><a href="mailto:guillaumedeloris@outlook.fr" target="blank">guillaumedeloris@outlook.fr</a></li>
             </ul>
         </div>
         <div class="transparence"></div>
